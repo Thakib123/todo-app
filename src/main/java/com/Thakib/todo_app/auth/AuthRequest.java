b22a4d639b35b4da47bac295de9f434ed1b97340
Kib Aroworowon <taroworowon@gmail.com>
2026-06-25 12:53:00 -0400

    Create AuthRequest.java

---
+++ src/main/java/com/Thakib/todo_app/auth/AuthRequest.java
@@ -0,0 +1,24 @@
+package com.Thakib.todo_app.auth;
+
+// A simple holder for the username + password coming in from register/login requests
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
